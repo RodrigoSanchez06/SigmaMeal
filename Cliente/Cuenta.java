5ca62a718871c7028d24003a4dc26fb5a9f237dd
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2022-11-15 17:31:32 -0600

    add: Serializacion y algunas cosas de cliente.

--- Cliente/Cuenta.java
+++ Cliente/Cuenta.java
@@ -1,22 +1,32 @@
 package Cliente;
 
+import java.io.Serializable;
+
 /**
  * Cuenta de puntos interna de SigmaMeal.
  */
-public class Cuenta {
+public class Cuenta implements Serializable {
 
     private long noCuenta;
     private String nip;
     private double saldo;
-    
+
+    public Cuenta(long noCuenta, String nip, double saldo) {
+        this.noCuenta = noCuenta;
+        this.nip = nip;
+        this.saldo = saldo;
+    }
+
     /**
      * Método que transfiere o ()
+     * 
      * @param cantidadTransferir
      * @param cuenta
      * @return
      */
-    public boolean trasferir(double cantidadTransferir, Cuenta cuenta){
-        if(cantidadTransferir <= 0) throw new IllegalArgumentException("No se pueden transferir cantidades negativas");
+    public boolean trasferir(double cantidadTransferir, Cuenta cuenta) {
+        if (cantidadTransferir <= 0)
+            throw new IllegalArgumentException("No se pueden transferir cantidades negativas");
         if (cantidadTransferir > this.saldo) {
             return false;
         } else {
@@ -26,13 +36,15 @@ public class Cuenta {
         }
     }
 
-    public void depositar(double cantidadDepositar){
-        if(cantidadDepositar <= 0) throw new IllegalArgumentException("No se pueden retirar cantidades negativas");
+    public void depositar(double cantidadDepositar) {
+        if (cantidadDepositar <= 0)
+            throw new IllegalArgumentException("No se pueden retirar cantidades negativas");
         saldo = saldo + cantidadDepositar;
     }
 
-    public boolean retirar(double cantidadRetirar){
-        if (cantidadRetirar  <= 0) throw new IllegalArgumentException("No se pueden retirar cantiades negativas");
+    public boolean retirar(double cantidadRetirar) {
+        if (cantidadRetirar <= 0)
+            throw new IllegalArgumentException("No se pueden retirar cantiades negativas");
         if (cantidadRetirar > saldo) {
             return false;
         } else {
@@ -41,7 +53,7 @@ public class Cuenta {
         }
     }
 
-    public double mostarSaldo(){
+    public double mostarSaldo() {
         return saldo;
     }
 }
