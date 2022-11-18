532bedb3adb508a47d4adbd825c9100e4d0d877f
RodrigoSanchez06 <rogerxdgta@ciencias.unam.mx>
2022-11-17 22:50:40 -0600

    add: no se

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-import Alimentos.Comida;
-import Alimentos.Predeterminadas.Carnivoro;
-import Batido.Batido;
-import Batido.Predeterminados.*;
 
 public class Main {
         public static void main(String[] args) {
